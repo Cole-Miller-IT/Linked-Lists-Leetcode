2924a221d54b0bfb09ec76f573384ecf06b38bd3
Cole-Miller-IT <68202945+Cole-Miller-IT@users.noreply.github.com>
2024-05-06 14:52:46 -0400

    Update removeNthNode.py

--- removeNthNode.py
+++ removeNthNode.py
@@ -1,3 +1,4 @@
+# Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
@@ -6,43 +7,41 @@
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        current = head
-        #print(current)
-        
+        #Count elements in the list
         counter = 0
         node = head
         while node != None:
             counter += 1
             node = node.next
             
-        arrLength = counter #5#len(head)
-        print("array length: " + str(arrLength))
+        listLength = counter #5#len(head)
+        #print("list length: " + str(listLength))
         
-        while arrLength > 0:
+        current = head
+        pos = 1 #Start at pos 1/the first element in the list
+        stopPos = listLength - n
+        #print("Stop Pos: " + str(stopPos))
+        
+        if stopPos == 0:
+            #remove the first element
+            head = head.next
+            return head
+        
+        #While the list is not empty
+        while listLength > 0:
             #If at the correct spot
-            ######stop at the predessosor, not at the node to remove
-            if n == arrLength:
-                print("At spot")
-                print(current)
-                #if we are at the last node
-                if current.next == None:
-                    print("last")
-                    if n == 1:
-                        head = None
-                    else:
-                        current = None
-                    
-                    
-                #at an inside node
-                else:
-                    print("inside")
-                    current.val = current.next.val
-                    current.next = current.next.next
-                    
-                print(current) 
-                return head
+            if pos == stopPos:
+                #print("at position " + str(pos))
+                #print(current)
+                
+                #Step over the element
+                current.next = current.next.next
+                #print(current)
+                break
                 
             else:
-                #Move forward
-                arrLength -= 1
+                #Take one step forward
+                pos += 1
                 current = current.next
+                
+        return head
